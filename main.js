7d12646c9a821307e4861bbfa8c848e68ec6bd3c
ababup1192 <ababup1192@gmail.com>
2016-09-21 23:14:07 +0900

    First commit

---
+++ main.js
@@ -0,0 +1,52 @@
+function createItem(text) {
+    var item = document.createElement("li");
+    var content = document.createTextNode(text);
+    item.appendChild(content);
+    return item;
+}
+
+function getStorageTweets() {
+    return Immutable.List(JSON.parse(localStorage.getItem("tweets")));
+}
+
+function setStorageTweet(list) {
+    localStorage.setItem("tweets", JSON.stringify(list))
+}
+
+function pushTweet(tweet) {
+    var tweetList = getStorageTweets();
+    setStorageTweet(tweetList.push(tweet));
+}
+
+function loadTweet() {
+    var tweetsList = document.getElementById("tweets");
+    tweetsList.innerHTML = "";
+
+    var tweets = getStorageTweets().reverse();
+    tweets.map(function (tweet) {
+        return createItem(tweet);
+    }).forEach(function (item) {
+        tweetsList.appendChild(item);
+    });
+}
+
+function init() {
+    loadTweet();
+
+    var tweetForm = document.getElementById("tweet");
+
+    tweetForm.addEventListener("keydown", function (e) {
+        var textValue = tweetForm.value;
+        if (e.keyCode === 13 && textValue !== "") {
+            pushTweet(textValue);
+            tweetForm.value = "";
+            loadTweet();
+        }
+    });
+}
+
+init();
+
+// 消す魔法
+// localStorage.clear();
+
